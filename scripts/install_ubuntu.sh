a169197fdb8d24655f1bedcb4f9d10b560382045
Sandwich <hi.na3s@gmail.com>
2019-01-26 14:57:33 -0500

    hotfix for 16.04

--- scripts/install_ubuntu.sh
+++ scripts/install_ubuntu.sh
@@ -14,7 +14,7 @@ curl -sL https://deb.nodesource.com/setup_10.x | bash -
 apt install -y nodejs
 
 # Get Binary Packages Information
-EOSIO_DOWNLOAD_URL=$(curl --silent "https://api.github.com/repos/EOSIO/eos/releases/latest" | grep "browser_download_url.*ubuntu-18.04_amd64.deb" | cut -d ":" -f 2,3 | tr -d \")
+EOSIO_DOWNLOAD_URL=$(curl --silent "https://api.github.com/repos/EOSIO/eos/releases/latest" | grep "browser_download_url.*ubuntu-16.04_amd64.deb" | cut -d ":" -f 2,3 | tr -d \")
 EOSIO_FILENAME=$(echo $EOSIO_DOWNLOAD_URL | sed 's:.*/::')
 
 CDT_DOWNLOAD_URL=$(curl --silent "https://api.github.com/repos/EOSIO/eosio.cdt/releases/latest" | grep "browser_download_url.*amd64.deb" | cut -d ":" -f 2,3 | tr -d \")

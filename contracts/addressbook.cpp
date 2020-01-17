8321aba5cf939f9c8519190a0190664819367cc3
Jeffrey Smith II <jeffreyssmith2nd@gmail.com>
2020-01-17 14:19:52 -0500

    Fix gitpod

--- contracts/addressbook.cpp
+++ contracts/addressbook.cpp
@@ -6,13 +6,13 @@ using namespace eosio;
 class [[eosio::contract("addressbook")]] addressbook : public eosio::contract {
 
 public:
-  
+
   addressbook(name receiver, name code,  datastream<const char*> ds): contract(receiver, code, ds) {}
 
   [[eosio::action]]
   void upsert(name user, std::string first_name, std::string last_name, uint64_t age, std::string street, std::string city, std::string state) {
-    require_auth( user );
-    address_index addresses(get_first_receiver(),get_first_receiver().value);
+    require_auth(user);
+    address_index addresses(get_first_receiver(), get_first_receiver().value);
     auto iterator = addresses.find(user.value);
     if( iterator == addresses.end() )
     {
@@ -25,6 +25,7 @@ public:
        row.city = city;
        row.state = state;
       });
+      send_summary(user, " successfully emplaced record to addressbook");
     }
     else {
       addresses.modify(iterator, user, [&]( auto& row ) {
@@ -36,6 +37,7 @@ public:
         row.city = city;
         row.state = state;
       });
+      send_summary(user, " successfully modified record to addressbook");
     }
   }
 
@@ -43,11 +45,18 @@ public:
   void erase(name user) {
     require_auth(user);
 
-    address_index addresses(get_self(), get_first_receiver().value);
+    address_index addresses(get_first_receiver(), get_first_receiver().value);
 
     auto iterator = addresses.find(user.value);
     check(iterator != addresses.end(), "Record does not exist");
     addresses.erase(iterator);
+    send_summary(user, " successfully erased record from addressbook");
+  }
+
+  [[eosio::action]]
+  void notify(name user, std::string msg) {
+    require_auth(get_self());
+    require_recipient(user);
   }
 
 private:
@@ -59,14 +68,23 @@ private:
     std::string street;
     std::string city;
     std::string state;
-  
+
     uint64_t primary_key() const { return key.value; }
-    uint64_t get_secondary_1() const { return age; }
-  
+    uint64_t get_secondary_1() const { return age;}
   };
 
+  void send_summary(name user, std::string message) {
+    action(
+      permission_level{get_self(),"active"_n},
+      get_self(),
+      "notify"_n,
+      std::make_tuple(user, name{user}.to_string() + message)
+    ).send();
+  };
+
+
   typedef eosio::multi_index<"people"_n, person,
-      indexed_by<"byage"_n, const_mem_fun<person, uint64_t, &person::get_secondary_1>>
-   > address_index;
-  
+    indexed_by<"byage"_n, const_mem_fun<person, uint64_t, &person::get_secondary_1>>
+  > address_index;
+
 };

21301c685a50c005309cb044250998ef4966de4c
Jeffrey Smith II <jeffreyssmith2nd@gmail.com>
2020-01-17 13:04:44 -0500

    Cleanup old stuff

--- start_frontend.sh
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env bash
-
-SOURCE_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
-
-# change to script's directory
-cd "${SOURCE_DIR}/frontend"
-
-echo "=== npm start ==="
-npm start
